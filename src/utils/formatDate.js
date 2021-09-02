b38131f75ce3025d37ec17a9b595ea7d175a0f3f
milankatira <milankatira26@gmail.com>
2021-09-02 13:00:00 +0530

    initial commit

--- src/utils/formatDate.js
+++ src/utils/formatDate.js
@@ -1,4 +1,16 @@
-export default (date) => {
+// export default ((date) => {
+//   const d = new Date(date);
+//   let month = `${d.getMonth() + 1}`;
+//   let day = `${d.getDate()}`;
+//   const year = d.getFullYear();
+
+//   if (month.length < 2) { month = `0${month}`; }
+//   if (day.length < 2) { day = `0${day}`; }
+
+//   return [year, month, day].join('-');
+// });
+
+const formatDate = (date) => {
   const d = new Date(date);
   let month = `${d.getMonth() + 1}`;
   let day = `${d.getDate()}`;
@@ -8,4 +20,29 @@ export default (date) => {
   if (day.length < 2) { day = `0${day}`; }
 
   return [year, month, day].join('-');
-};
+}
+
+export default formatDate;
+// const date = () => {
+//   const d = new Date(date);
+//   let month = `${d.getMonth() + 1}`;
+//   let day = `${d.getDate()}`;
+//   const year = d.getFullYear();
+
+//   if (month.length < 2) { month = `0${month}`; }
+//   if (day.length < 2) { day = `0${day}`; }
+
+//   return [year, month, day].join('-');
+// };
+
+// export default date;
+
+// const formatDate = () => {
+//   return (
+//     <div>
+
+//     </div>
+//   )
+// }
+
+// export default formatDate
